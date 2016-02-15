367a7f1558a73009b94b61ab5b4d9fcf7118e4de
Kuba Pilecki <k.pilecki@gmail.com>
2016-02-15 17:34:51 +0100

    :)

--- package.sh
+++ package.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+NOW=$(date)
+BUILD="Build: $2:$3 ($NOW)"
 TARGET_DIR=$1
 
 if [ -z "$1" ]; then TARGET_DIR="."; fi
 
 TARGET_FILE="$TARGET_DIR/Magento_Invipay_Paygate.tgz"
 
+echo "Packaging Magento Plugin ($BUILD) into: $TARGET_FILE"
+
 if [ -f "$TARGET_FILE" ]; then rm "$TARGET_FILE"; fi
 
-echo "Packaging Magento plugin into: $TARGET_FILE"
+echo "$BUILD" > app/code/community/Invipay/VERSION.md
 tar -zchvf "$TARGET_FILE" --exclude='.DS_Store' --exclude='.git' --exclude='.gitmodules' app
