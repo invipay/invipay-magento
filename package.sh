7f5590de39fef8ba28f7ddf191e9919f40400cbe
Kuba Pilecki <k.pilecki@gmail.com>
2016-02-15 17:15:48 +0100

    Better package script

--- package.sh
+++ package.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-tar -zchvf ../Magento_Invipay_Paygate.tgz --exclude='.DS_Store' app
+
+TARGET_DIR=$1
+
+if [ -z "$1" ]; then TARGET_DIR="."; fi
+
+TARGET_FILE="$TARGET_DIR/Magento_Invipay_Paygate.tgz"
+
+if [ -f "$TARGET_FILE" ]; then rm "$TARGET_FILE"; fi
+
+echo "Packaging Magento plugin into: $TARGET_FILE"
+tar -zchvf "$TARGET_FILE" --exclude='.DS_Store' --exclude='.git' --exclude='.gitmodules' app
